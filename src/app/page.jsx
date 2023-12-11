b713fe4c31d208b2d27f93df407f417714081b92
Sumit Chakraborty <96117153+EmptyNode@users.noreply.github.com>
2023-12-11 22:41:33 +0530

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,8 @@
 import styles from "./homepage.module.css";
 
 export default function Home() {
-  return <div>Hello</div>;
+  return <div>
+    <h1>this is a blog app created using next js</h1>  
+  </div>;
   // return <div>Hello</div>;
 }
